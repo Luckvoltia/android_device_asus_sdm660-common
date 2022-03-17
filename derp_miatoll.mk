7fdcd3a9e4bcead334778f3d1b7c473d815c8a53
Luckvoltia <luckvoltia18@gmail.com>
2022-03-17 08:26:46 +0000

    miatoll: arrow -> derp

--- derp_miatoll.mk
+++ derp_miatoll.mk
@@ -7,9 +7,13 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common derpfest stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+
+DERP_BUILDTYPE := Official
+USE_LEGACY_BOOTANIMATION := true
+TARGET_USES_BLUR := true
 
 # Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -18,16 +22,13 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := derp_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone git@github.com:ArrowOS-Devices/android_kernel_xiaomi_sm6250.git -b arrow-12.1 kernel/xiaomi/sm6250
-git clone git@github.com:ArrowOS-Devices/android_device_xiaomi_sm6250-common.git -b arrow-12.1 device/xiaomi/sm6250-common
+git clone git@github.com:Luckvoltia/android_device_asus_sdm660-common.git -b sm6250-common device/xiaomi/sm6250-common
 git clone git@github.com:ArrowOS-Devices/android_vendor_xiaomi_miatoll.git -b arrow-12.1 vendor/xiaomi/miatoll
 git clone git@github.com:ArrowOS-Devices/android_vendor_xiaomi_sm6250-common.git -b arrow-12.1 vendor/xiaomi/sm6250-common
 git clone git@github.com:ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
